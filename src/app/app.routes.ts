dec5739c8838e66392e22adad01e1558baccce72
Rajveer Singh <rajveersingh.charan@gmail.com>
2025-02-08 11:10:41 +0530

    Setup Navigation Bar to the Application with atractive css and responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    {   path: 'signup', component: SignupComponent },
     {   path: 'login', component: LoginComponent },
+    {   path: 'signup', component: SignupComponent },
 ];
