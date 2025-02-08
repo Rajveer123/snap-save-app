38eb661c0f21110e6da5a2a7763afb85a0a394c8
Rajveer Singh <rajveersingh.charan@gmail.com>
2025-02-08 13:01:14 +0530

    Added other required components in to application.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
+import { CreateBinComponent } from './create.bin/create.bin.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
     {   path: 'login', component: LoginComponent },
     {   path: 'signup', component: SignupComponent },
+    {   path: 'about', component:  AboutComponent},
+    {   path: 'createbin', component: CreateBinComponent },
+    {   path: 'logout', component: LogoutComponent,  pathMatch: 'full' },
+    
+    
 ];
---
+++ src/app/create.bin/create.bin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create.bin',
+  imports: [],
+  templateUrl: './create.bin.component.html',
+  styleUrl: './create.bin.component.css'
+})
+export class CreateBinComponent {
+
+}
