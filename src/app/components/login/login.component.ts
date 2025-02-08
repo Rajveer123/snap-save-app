0b3213042f5caad40177734f880b7285bcaa24ab
Rajveer Singh <rajveersingh.charan@gmail.com>
2025-02-08 15:19:39 +0530

    Set UI for the login screen

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule], 
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+username = new FormControl("",[Validators.required, Validators.email]) ;
+password = new FormControl("",[Validators.required, Validators.minLength(6)]);
+loginForm = new FormGroup({
+  usernames: this.username,
+  password: this.password
+});
+handleLogin(){
+console.log('form values :',this.loginForm.value);
+}
+handleReset(){
+  this.loginForm.reset();
+}
 }
